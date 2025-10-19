426752b99a02ed4f591ca731829f6c83da2c969f
Ngoctam2407 <mysunny2407@gmail.com>
2025-10-19 22:22:03 +0700

    Add files via upload

---
+++ DPM225470_NguyenNgocTam_Patterns_20_State/DPM225470_NguyenNgocTam_Patterns_20_State/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DPM225470_NguyenNgocTam_Patterns_20_State
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Setup context in a state
+            var context = new Context(new ConcreteStateA());
+            // Issue requests, which toggles state
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
